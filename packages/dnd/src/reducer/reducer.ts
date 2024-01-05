62ef1c591356edca925b5e42b3c8978532cc226d
webdev-codeline <rain116093@hotmail.com>
2024-01-05 22:35:10 +0330

    add logs to if conditions

--- packages/dnd/src/reducer/reducer.ts
+++ packages/dnd/src/reducer/reducer.ts
@@ -30,8 +30,16 @@ export const dragStart = (state: StateContext, action: ReducerAction) => ({
 });
 
 export const dragDrop = (state: StateContext, action: ReducerAction) => {
+	console.debug("STARTED DRAG_DROP")
+	console.log("STARTED DRAG_DROP")
+	console.warn("STARTED DRAG_DROP")
+	console.error("STARTED DRAG_DROP")
+
+
 	// dragging outside container
 	if (!action.targetContainer) {
+		console.log("dragging outside container")
+
 		return {
 			...state,
 			...clearDrag,
@@ -42,6 +50,8 @@ export const dragDrop = (state: StateContext, action: ReducerAction) => {
 
 	// dragging container into itself
 	if (isDraggingContainer && state.draggingElement === action.targetContainer) {
+		console.log("dragging container into itself")
+
 		return {
 			...state,
 			...clearDrag,
@@ -58,8 +68,10 @@ export const dragDrop = (state: StateContext, action: ReducerAction) => {
 			parentContainer = Object.keys(state.containers).find(key => state.containers[key].children.includes(parentContainer as string));
 			console.log('parent', parentContainer);
 		}
+		console.log("dragging container into sub-container [IF FALSE]")
 
 		if (parentContainer === state.draggingElement) {
+			console.log("dragging container into sub-container [IF TRUE]")
 			return {
 				...state,
 				...clearDrag,
@@ -77,6 +89,8 @@ export const dragDrop = (state: StateContext, action: ReducerAction) => {
             state.containers[state.sourceContainer].children.indexOf(state.draggingElement as string) + 1 === action.position
         )
 	) {
+		console.log("is moving to the same position")
+
 		return {
 			...state,
 			...clearDrag,
@@ -86,27 +100,40 @@ export const dragDrop = (state: StateContext, action: ReducerAction) => {
 	// generate id for new items or used the old one
 	const id = action.id || state.sourceContainer ? state.draggingElement : `${state.draggingElement}-${Date.now()}`;
 
+	console.log(`Generated ID: ${id}`)
+
 	// remove item from the old container, or create a new one
 	if (state.sourceContainer) {
+		console.log("remove item from the old container, or create a new one")
+
 		state.containers[state.sourceContainer].children = state.containers[state.sourceContainer].children.filter((child: string) => child !== id);
 	}
 
 	// push to the exact position
 	// when position is not specified, push to the bottom
 	if (typeof action.position === 'undefined') {
+		console.log("push to the bottom")
+
 		state.containers[action.targetContainer].children.push(id as string);
 	} else {
+		console.log(`push to the selected position`)
+
 		state.containers[action.targetContainer].children.splice(action.position, 0, id as string);
 	}
 
 	// moving existing node
 	if (state.sourceContainer) {
+		console.log("moving existing node")
+
 		return {
 			...state,
 			...clearDrag,
 		};
 		// inserting a new node
 	} else {
+		console.log("inserting a new node")
+		console.log("if dragging component add a component")
+
 		// if dragging component add a component
 		return {
 			...state,
